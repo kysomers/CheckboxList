0c1ccade8d6e9349858876e1a49d169d659607cc
kysomers <kyledouglassomers@gmail.com>
2015-12-01 10:28:42 -0800

    first commit

---
+++ CheckboxList.podspec
@@ -0,0 +1,38 @@
+Pod::Spec.new do |s|
+
+# 1
+s.platform = :ios
+s.ios.deployment_target = '8.0'
+s.name = "CheckboxList"
+s.summary = "CheckboxList allows checkboxes for simple user input when given a short list of options."
+s.requires_arc = true
+
+# 2
+s.version = "0.1.0"
+
+# 3
+s.license = { :type => "MIT", :file => "LICENSE" }
+
+# 4 - Replace with your name and e-mail address
+s.author = { "Kyle Somers" => "kyledouglassomers@gmail.com" }
+
+
+# 5 - Replace this URL with your own Github page's URL (from the address bar)
+s.homepage = "https://github.com/kysomers/CheckboxList"
+
+
+
+# 6 - Replace this URL with your own Git URL from "Quick Setup"
+s.source = { :git => "https://github.com/kysomers/CheckboxList.git", :tag => "#{s.version}"}
+
+
+# 7
+s.framework = "UIKit"
+
+
+# 8
+s.source_files = "CheckboxListSwift/**/*.{swift}"
+
+# 9
+s.resources = "CheckboxListSwift/**/*.{png}"
+end
---
+++ CheckboxListSwift/CheckBoxButton.swift
@@ -0,0 +1,127 @@
+//
+//  CheckBoxButton.swift
+//  SoKi
+//
+//  Created by Kyle Somers on 11/1/15.
+//  Copyright © 2015 Siddharth Atre. All rights reserved.
+//
+
+import UIKit
+
+class CheckBoxButton: UIButton {
+
+    
+
+    let nameLabel = UILabel()
+    let checkBox = UIImageView()
+    var checkedImage = UIImage(named: "CheckedBox")
+    var uncheckedImage = UIImage(named: "UncheckedBox")
+    var isOn = false
+    var setupDone = false
+    let fontSize : CGFloat = 18
+    let smallMargin : CGFloat = 8
+    var buttonColor = UIColor.flatRedColor()
+    var buttonFont = UIFont()
+    
+    /**
+    This will set up the box and set its name
+    
+    - parameter boxName: the name associated with the checkbox
+    */
+    func setName(boxName : String){
+        
+        if setupDone == false{
+            self.addSubview(nameLabel)
+            self.addSubview(checkBox)
+            self.addTarget(self, action: "toggle:", forControlEvents: .TouchUpInside)
+            setupDone = true
+        }
+        
+        nameLabel.text = boxName
+        nameLabel.font = buttonFont
+        nameLabel.numberOfLines = 1
+        nameLabel.sizeToFit()
+        
+        checkBox.image = uncheckedImage
+        checkBox.frame = CGRectMake(0, 0, nameLabel.frame.height, nameLabel.frame.height)
+        
+        nameLabel.frame.origin = CGPointMake(checkBox.frame.width + smallMargin, 0)
+        self.frame.size = CGSizeMake(checkBox.frame.width + smallMargin + nameLabel.frame.width, nameLabel.frame.height)
+        
+        
+        
+    }
+    
+    
+    /**
+    toggles the button on and off
+    
+    - parameter sender: always self
+    */
+    func toggle(sender: AnyObject){
+        if isOn == true{
+            turnOff()
+        }
+        else{
+            turnOn()
+        }
+        
+    }
+    /**
+    Checks a box
+    */
+    func turnOn(){
+        isOn = true
+        nameLabel.textColor = buttonColor
+        //nameLabel.font = UIFont(name: "AvenirNext-DemiBold", size: fontSize)
+        nameLabel.sizeToFit()
+        
+        checkBox.image = checkedImage
+        checkBox.image = checkBox.image!.imageWithRenderingMode(.AlwaysTemplate)
+        checkBox.tintColor = buttonColor
+    }
+    /**
+    unchecks a box
+    */
+    func turnOff(){
+        isOn = false
+        nameLabel.textColor = UIColor.blackColor()
+        //nameLabel.font = UIFont(name: "Avenir Next", size: fontSize)
+        nameLabel.sizeToFit()
+        checkBox.image = uncheckedImage
+        checkBox.tintColor = UIColor.blackColor()
+
+    }
+    /**
+    Sets the color
+    
+    - parameter theColor: The color that you want to set that button to
+    */
+    func setColor(theColor : UIColor){
+        buttonColor = theColor
+        if isOn{
+            nameLabel.textColor = buttonColor
+            checkBox.image = checkedImage
+            checkBox.image = checkBox.image!.imageWithRenderingMode(.AlwaysTemplate)
+            checkBox.tintColor = buttonColor
+        }
+    }
+    
+    func setShape( shape : NSString){
+        if shape.lowercaseString == "circle"{
+            checkedImage = UIImage(named: "CheckedCircle")
+            uncheckedImage = UIImage(named: "UncheckedCircle")
+        }
+        if isOn{
+            checkBox.image = checkedImage
+            checkBox.image = checkBox.image!.imageWithRenderingMode(.AlwaysTemplate)
+            checkBox.tintColor = buttonColor
+        }
+        else{
+            checkBox.image = uncheckedImage
+
+        }
+    }
+    
+
+}
